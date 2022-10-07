bb89cdaec3eeb8fcf4f995a18d9ed11c670aba3c
User <dg21stew@gmail.com>
2022-10-06 23:43:01 -0700

    Print getText

--- src/main/java/RestApiTutorial.java
+++ src/main/java/RestApiTutorial.java
@@ -7,7 +7,7 @@ import java.net.http.HttpResponse;
 
 public class RestApiTutorial {
 
-    public static void main(String[] args) throws Exception{
+    public static void main(String[] args) throws Exception {
 
         Transcript transcript = new Transcript();
         transcript.setAudio_url("https://raw.githubusercontent.com/johnmarty3/JavaAPITutorial/main/Thirsty.mp4");
@@ -28,6 +28,28 @@ public class RestApiTutorial {
 
         System.out.println(postResponse.body());
 
-    }
+        transcript = gson.fromJson(postResponse.body(), Transcript.class);
 
+        System.out.println(transcript.getId());
+
+        HttpRequest getRequest = HttpRequest.newBuilder()
+                .uri(new URI("https://api.assemblyai.com/v2/transcript/" + transcript.getId()))
+                .header("Authorization", "d1d9ee070dea4a9e977ac5a8c4266bfe")
+                .build();
+
+        while (true) {
+            HttpResponse<String> getResponse = httpClient.send(getRequest, HttpResponse.BodyHandlers.ofString());
+            transcript = gson.fromJson(postResponse.body(), Transcript.class);
+
+            System.out.println(transcript.getStatus());
+
+            if ("completed".equals(transcript.getStatus()) || "error".equals(transcript.getStatus())) {
+                break;
+            }
+
+            Thread.sleep(1000);
+        }
+        System.out.println("Transcription completed");
+        System.out.println(transcript.getText());
+    }
 }
